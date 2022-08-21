195519b84b730a89119714a9fd09cfb0d36cfb1e
nikob <nikolaj.bauer@uzh.ch>
2022-08-21 20:01:01 +0200

    get rid of sklearn package by implementing own label encoder, include a docstring for function inference()

--- BERT/run_BERT_classifier_onnx.py
+++ BERT/run_BERT_classifier_onnx.py
@@ -3,36 +3,58 @@
 
 # Author: Nikolaj Bauer
 
-import time
-importtime_0 = time.perf_counter()
-import torch
-import numpy as np
-from transformers import BertTokenizer
-import onnxruntime as ort
-from sklearn.preprocessing import LabelEncoder
+import sys
 import os
-importtime_1 = time.perf_counter()
 
-# only use this while testing on existing dataset...
-# takes 3.3 seconds to import...
-from run_BERT_classifier import get_input_data
+import time
+import numpy as np
+# import math  # if we one day can get around using numpy...
+from transformers import DistilBertTokenizer as BertTokenizer
+import onnxruntime as ort
+# from sklearn.preprocessing import LabelEncoder
 
-
-
-
-
+directory = os.path.dirname(__file__)
+sys.path.append(directory)
+models_path = os.path.join(directory, "distilBert_news_model.onnx")
+tokenizer_path = os.path.join(directory, 'tokenizer')
 
 
 def softmax(x):
+    """calculate the softmax of an array
+    @:param x: nparray or list with numeric values
+    @:return: numpy.ndarray"""
     return np.exp(x)/sum(np.exp(x))
 
+# def softmax(x):
+#     exponents = []
+#     for element in x:
+#         exponents.append(math.exp(element))
+#     summ = sum(exponents)
+#     for i in range(len(exponents)):
+#         exponents[i] = exponents[i] / summ
+#     return exponents
 
-tokenizer = BertTokenizer.from_pretrained("./tokenizer")
+class LabelEncoder():
+    """Simple label encoder so I do not have to import the whole sklearn package.
+    For our case it should behave exactly the same as sklearn.preprocessing.LabelEncoder"""
+    def __init__(self, labels:list):
+        self.labels = sorted(labels)
+
+    def encode(self, element: str) -> int:
+        return self.labels.index(element)
+
+    def decode(self, idx: int) -> str:
+        return self.labels[idx]
+
+
+tokenizer = BertTokenizer.from_pretrained(tokenizer_path)
+
+
 def preprocessing_for_bert(data):
     """Perform required preprocessing steps for BERT.
     @param    data (np.array): Array of texts to be processed.
-    @return   input_ids (torch.Tensor): Tensor of token ids to be fed to a model.
-    @return   attention_masks (torch.Tensor): Tensor of indices specifying which
+    @return   input_ids (list[list]): List of token ids for each input sentence in data
+    @return   attention_masks (list[list]): List of attention masks for each input sentence in data
                   tokens should be attended to by the model.
     """
     # Create empty lists to store outputs
@@ -41,7 +63,6 @@ def preprocessing_for_bert(data):
 
     # For every sentence...
     for sent in data:
-        # cut too long sentences, only take the end
 
         # `encode_plus` will:
         #    (1) Tokenize the sentence
@@ -64,51 +85,56 @@ def preprocessing_for_bert(data):
         input_ids.append(encoded_sent.get('input_ids'))
         attention_masks.append(encoded_sent.get('attention_mask'))
 
-    # Convert lists to tensors
-    input_ids = torch.tensor(input_ids)
-    attention_masks = torch.tensor(attention_masks)
+    # we do not even need nd.arrays
+    # Convert lists to ndarrays (since we are using onnx we do not need tensors)
+    # input_ids = np.asarray(input_ids, dtype=np.int64)
+    # attention_masks = np.asarray(attention_masks, dtype=np.int64)
 
     return input_ids, attention_masks
 
 
+def inference(text, model=models_path):
+    """perform a forward pass on a model that is saved as onnx
+    :param model: a filepath to a .onnx file
+    :param text: a string to be passed to the model
+    :return: the logits as a numpy array"""
 
-
-def inference(model, text):
-    time_start_sess_0 = time.perf_counter()
     ort_session = ort.InferenceSession(model)
-    time_start_sess_1 = time.perf_counter()
-    print(f"time to start inf-sess: {time_start_sess_1-time_start_sess_0}")
 
-    time_start_pred_0 = time.perf_counter()
     inputs = preprocessing_for_bert([text])
     outputs = ort_session.run(
         None,
-        {"input_ids": inputs[0].numpy(), "input_masks":inputs[1].numpy()},
+        {"input_ids": inputs[0], "input_masks":inputs[1]},
     )
-    time_start_pred_1 = time.perf_counter()
-    print(f"time to start predict: {time_start_pred_1 - time_start_pred_0}")
 
     return outputs[0][0]
 
+
 def get_prediction(logits, le):
-    # print(outputs)  # list with one element: Numpy array of the logits
+    """Transform the output of the model into a prediction
+    :param logits: a numpy array, the output of a forward pass
+    :param le: a LabelEncoder that is used to decode the label
+    :return: string of the label that the model predicts"""
+
     probs = softmax(logits)
     pred = np.argmax(probs)
-    return le.inverse_transform([pred])[0]
+    return le.decode(pred)
+
+    # with the sklearn LabelEncoder:
+    # return le.inverse_transform([pred])[0]
+
 
 if __name__=="__main__":
-    print(f"importing time: {importtime_1 - importtime_0}")
+
     news_classes = ['entertainment', 'politics', 'tech', 'sport', 'business']
     num_labels_news = len(news_classes)
     le_news = LabelEncoder()
     le_news.fit(news_classes)
 
-    # only for testing the news dataset, in the End we should just put a string as input
-    label, text = get_input_data("data/bbc-text_test.csv", 445)
+    label, text = ("business", "This is an article about the FED raising interest rates in the upcoming quater. Goldman Sachs executives criticized this move.")
     print("start inference...")
     time_0 = time.perf_counter()
-    model = os.path.abspath(r"C:\Users\nik_b\Documents\UZH\Party_Game\models\news_model.onnx")
-    logits = inference(model, text)
+    logits = inference(text, models_path)
     time_1 = time.perf_counter()
 
     prediction = get_prediction(logits, le_news)
